87424268bedf872c304098172ec9a2f0b675a76f
Gregory McWhirter <greg@ideafreemonoid.org>
2024-07-13 20:41:26 -0400

    testing

--- cmd/fyne/internal/commands/runner.go
+++ cmd/fyne/internal/commands/runner.go
@@ -34,7 +34,7 @@ func (c *command) runOutput(arg ...string) ([]byte, error) {
 		cmd.Env = c.env
 		c.env = []string{}
 	}
-	fmt.Fprintf(os.Stderr, "!!! %s %v env={%v} dir=%s", c.exe, arg, cmd.Env, cmd.Dir)
+	fmt.Fprintf(os.Stderr, "!!! %s %v env={%v} dir=%s\n", c.exe, arg, cmd.Env, cmd.Dir)
 	return cmd.CombinedOutput()
 }
 
